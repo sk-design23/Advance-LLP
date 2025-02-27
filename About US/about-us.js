e21f6de9363fea87a84dea1fbaa6b49127174f4b
sk-design23 <hellosonu620@gmail.com>
2025-02-28 01:34:55 +0530

    New

---
+++ About US/about-us.js
@@ -0,0 +1,4 @@
+// Add interactivity to the "Contact Us" button
+document.getElementById('contactBtn').addEventListener('click', function () {
+    alert('Thank you for reaching out! Please email us at info@arm-mfg.com.');
+  });
